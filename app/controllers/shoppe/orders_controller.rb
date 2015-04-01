7ac7fe5df548e299c357035b093ab90dfaba56f7
tochman <thomas@necebo.se>
2015-04-01 21:03:38 +0200

    ui updates and added customer create method on order accept!

--- app/controllers/shoppe/orders_controller.rb
+++ app/controllers/shoppe/orders_controller.rb
@@ -2,7 +2,7 @@ module Shoppe
   class OrdersController < Shoppe::ApplicationController
 
     before_filter { @active_nav = :orders }
-    before_filter { params[:id] && @order = Shoppe::Order.find(params[:id])}
+    before_filter { params[:id] && @order = Shoppe::Order.find(params[:id]) }
 
     def index
       @query = Shoppe::Order.ordered.received.includes(:order_items => :ordered_item).page(params[:page]).search(params[:q])
@@ -51,6 +51,7 @@ module Shoppe
 
     def accept
       @order.accept!(current_user)
+      create_customer(@order)
       redirect_to @order, :notice => t('shoppe.orders.accept_notice')
     rescue Shoppe::Errors::PaymentDeclined => e
       redirect_to @order, :alert => e.message
@@ -76,16 +77,28 @@ module Shoppe
 
     def safe_params
       params[:order].permit(
-        :customer_id,
-        :first_name, :last_name, :company,
-        :billing_address1, :billing_address2, :billing_address3, :billing_address4, :billing_postcode, :billing_country_id,
-        :separate_delivery_address,
-        :delivery_name, :delivery_address1, :delivery_address2, :delivery_address3, :delivery_address4, :delivery_postcode, :delivery_country_id,
-        :delivery_price, :delivery_service_id, :delivery_tax_amount,
-        :email_address, :phone_number,
-        :notes,
-        :order_items_attributes => [:ordered_item_id, :ordered_item_type, :quantity, :unit_price, :tax_amount, :id, :weight]
+          :customer_id,
+          :first_name, :last_name, :company,
+          :billing_address1, :billing_address2, :billing_address3, :billing_address4, :billing_postcode, :billing_country_id,
+          :separate_delivery_address,
+          :delivery_name, :delivery_address1, :delivery_address2, :delivery_address3, :delivery_address4, :delivery_postcode, :delivery_country_id,
+          :delivery_price, :delivery_service_id, :delivery_tax_amount,
+          :email_address, :phone_number,
+          :notes,
+          :order_items_attributes => [:ordered_item_id, :ordered_item_type, :quantity, :unit_price, :tax_amount, :id, :weight]
       )
+    end
+
+    def create_customer(order)
+      unless Shoppe::Customer.exists?(first_name: order.first_name, last_name: order.last_name)
+        customer = Shoppe::Customer.new
+        customer.first_name = order.first_name
+        customer.last_name = order.last_name
+        customer.company = order.company
+        customer.email = order.email_address
+        customer.phone = order.phone_number
+        customer.save!
+      end
     end
   end
 end
